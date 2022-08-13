dce89e0eaaf27f312005c34f60f2546e4b419bd9
Walid-Abbes <walid.abbes@etu.usthb.dz>
2022-08-13 12:43:40 +0200

    JS commit

---
+++ Java script/main.js
@@ -0,0 +1,56 @@
+// comments
+/*cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc*/
+
+/* 
+primitive :
+number
+bool
+*/
+let challenge = "full stack js track";
+console.log(challenge);
+console.log(challenge.length);
+console.log(challenge.toUpperCase());
+console.log(challenge.toLowerCase());
+console.log(challenge.substring(5));
+console.log(challenge.substring());
+console.log(challenge.includes("stack"));
+console.log(challenge.split(" "));
+console.log(challenge.split(""));
+console.log(challenge.split("a"));
+console.log(challenge.replace("track", "groupe"));
+
+const cuz =
+  "You cannot end a sentence with because because because is a conjunction";
+console.log(cuz.search("because"));
+
+var vr = "     hhhhhhhhhhhhhhhhhh hhhhhhhhhhh    ";
+console.log(vr);
+console.log(vr.trim());
+console.log(challenge.startsWith());
+console.log(challenge.startsWith("f"));
+console.log(challenge.endsWith(""));
+console.log(challenge.endsWith("k"));
+let kk = "30 days of JavaScript";
+console.log(kk.match("a"));
+a = "full stack";
+b = " JavaScript";
+console.log(a.concat(b));
+const V = "Desuwa";
+console.log(V.repeat(999));
+///////////
+console.log(
+  "There is no exercise better for the heart than reaching down and lifting people up. by John Holmes teaches us to help one another"
+);
+console.log(
+  "Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead."
+);
+
+let gog = "python";
+let magog = "jargon";
+
+console.log(gog.includes("on"));
+console.log(magog.includes("on"));
+console.log(Math.floor(Math.random() * 101));
+
+const rndInt = Math.floor(Math.random() * 101) + 50
+console.log(rndInt)
