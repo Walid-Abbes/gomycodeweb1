a06fa8df6b352cdd0f0d626eb32fd301b820354b
Walid-Abbes <walid.abbes@etu.usthb.dz>
2022-09-03 09:26:56 +0200

    mmm

--- Java script/main.js
+++ Java script/main.js
@@ -81,21 +81,114 @@
 
 // do {} while (condition);
 
-let empty = [];
-let arr = [1, 2, 3, 5, 6, 7, 8, 9];
-console.log(empty.length);
-console.log(arr.length);
-let x = arr.length / 2;
+// let empty = [];
+// let arr = [1, 2, 3, 5, 6, 7, 8, 9];
+// console.log(empty.length);
+// console.log(arr.length);
+// let x = arr.length / 2;
 
-console.log(arr.slice(x - 1, x));
-console.log(arr.slice(0, 1));
-console.log(arr.slice(arr.length - 1, arr.length));
+// console.log(arr.slice(x - 1, x));
+// console.log(arr.slice(0, 1));
+// console.log(arr.slice(arr.length - 1, arr.length));
 
-let mixedData = [
-  "king",
-  55,
-  true,
-  (obb = { price: 1000, type: "heavyWater", molecular: "HD03" }),
-];
-console.log(mixedData.length);
-boss;
+// let mixedData = [
+//   "king",
+//   55,
+//   true,
+//   (obb = { price: 1000, type: "heavyWater", molecular: "HD03" }),
+// ];
+// console.log(mixedData.length);
+// boss;
+
+// let dog = {};
+// dog.name = "spike";
+// dog.breed = "bulldog";
+// dog.color = "grey";
+// dog.legs = 4;
+// console.log(dog);
+
+const users = {
+  Alex: {
+    email: "alex@alex.com",
+    skills: ["HTML", "CSS", "JavaScript"],
+    age: 20,
+    isLoggedIn: false,
+    points: 30,
+  },
+  Asab: {
+    email: "asab@asab.com",
+    skills: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "Redux",
+      "MongoDB",
+      "Express",
+      "React",
+      "Node",
+    ],
+    age: 25,
+    isLoggedIn: false,
+    points: 50,
+  },
+  Brook: {
+    email: "daniel@daniel.com",
+    skills: ["HTML", "CSS", "JavaScript", "React", "Redux"],
+    age: 30,
+    isLoggedIn: true,
+    points: 50,
+  },
+  Daniel: {
+    email: "daniel@alex.com",
+    skills: ["HTML", "CSS", "JavaScript", "Python"],
+    age: 20,
+    isLoggedIn: false,
+    points: 40,
+  },
+  John: {
+    email: "john@john.com",
+    skills: ["HTML", "CSS", "JavaScript", "React", "Redux", "Node.js"],
+    age: 20,
+    isLoggedIn: true,
+    points: 50,
+  },
+  Thomas: {
+    email: "thomas@thomas.com",
+    skills: ["HTML", "CSS", "JavaScript", "React"],
+    age: 20,
+    isLoggedIn: false,
+    points: 40,
+  },
+  Paul: {
+    email: "paul@paul.com",
+    skills: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "MongoDB",
+      "Express",
+
+      "React",
+      "Node",
+    ],
+    age: 20,
+    isLoggedIn: false,
+    points: 40,
+  },
+};
+
+// function getMaxSKill() {
+//   let max = 0;
+//   let userName = "";
+//   for (const user in users) {
+//     if (users[user].skill.length > max) {
+//       userName = users[user];
+//     }
+//   }
+//   return { user: userName };
+// }
+// getMaxSKill();
+
+function putName(user, users,) {
+  
+}
