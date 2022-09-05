5d243fdfdb180ad71f0c198c18352d9b3363caf9
Walid-Abbes <walid.abbes@etu.usthb.dz>
2022-09-05 09:46:54 +0200

    mmm

---
+++ DOM/dom/main.js
@@ -0,0 +1,40 @@
+// const counter = document.querySelector(".counter");
+// const inc = document.querySelector(".inc");
+// const dec = document.querySelector(".dec");
+// const ref = document.querySelector(".ref");
+// let count = 0;
+
+// function incc() {
+//   count++;
+//   counter.textContent = count;
+// }
+// function decc() {
+//   if (count == 0) {
+//     return;
+//   }
+//   count--;
+//   counter.textContent = count;
+// }
+// function reff() {
+//   counter.textContent = 0;
+// }
+// inc.addEventListener("click", incc);
+// dec.addEventListener("click", decc);
+// ref.addEventListener("click", reff);
+
+// function changeStyle() {
+//   let element = document.getElementById("container");
+//   element.style.display = "none";
+// }
+// function changeStyle2() {
+//   let element = document.getElementById("container");
+//   element.style.display = "block";
+// }
+
+function DEL() {}
+function AC() {}
+function plus() {}
+function minus() {}
+function product() {}
+function division() {}
+function equal() {}
